4ec13388245e1c5f054638d8757ea5a371af8ef3
garfield <cmgarfield@gmail.com>
2018-12-27 14:54:54 +0800

    Import Provider and wrap the App for data access
    Provider ensures you can use connect to pass data from the store to one
    or more React components.
    
    Listing 2.10. src/index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import tasks from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(tasks);
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
