df202cc53feab3f550de9987a3c285177af983cc
garfield <cmgarfield@gmail.com>
2018-12-27 13:57:31 +0800

    Create a store with 'tasks' reducer
    Listing 2.7. src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import tasks from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(tasks);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
