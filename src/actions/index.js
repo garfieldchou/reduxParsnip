099e193a622eecc15dc7de6463c4201528d14e65
garfield <cmgarfield@gmail.com>
2019-01-24 14:13:58 +0800

    Add a new fetchTasksStarted action creator
    First, dispatch the fetchTasksStarted action creator to signify a
    request is in progress.
    Second, use setTimeout to ensures the loading indicator will stay on
    the page for more than a fraction of a second.
    
    Listing 4.15. src/actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,12 +14,22 @@ export function fetchTasksSucceeded(tasks) {
   }
 }
 
+function fetchTasksStarted() {
+  return {
+    type: 'FETCH_TASKS_STARTED',
+  };
+}
+
 export function fetchTasks() {
   return dispatch => {
+    dispatch(fetchTasksStarted());
+
     api.fetchTasks().then(resp => {
-      dispatch(fetchTasksSucceeded(resp.data));
+      setTimeout(() => {
+        dispatch(fetchTasksSucceeded(resp.data));
+      }, 2000);
     });
-  }
+  };
 }
 
 function createTaskSucceeded(task) {
