32720d74224e26eca795d8a32d9ffa8b51124246
garfield <cmgarfield@gmail.com>
2019-01-24 11:29:16 +0800

    Update task reducer to take a new initial state
    Update the tasks reducer to take a new initial state, and update the
    existing action handlers.
    
    Listing 4.13. src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,9 @@
-export default function tasks(state = { tasks: [] }, action) {
+const initialState = {
+  tasks: [],
+  isLoading: false,
+};
+
+export default function tasks(state = initialState, action) {
   switch (action.type) {
     case 'CREATE_TASK': {
       return {
@@ -19,24 +24,29 @@ export default function tasks(state = { tasks: [] }, action) {
     }
     case 'FETCH_TASKS_SUCCEEDED': {
       return {
+        ...state,
+        isLoading: false,
         tasks: action.payload.tasks,
       };
     }
     case 'CREATE_TASK_SUCCEEDED': {
       return {
+        ...state,
         tasks: state.tasks.concat(action.payload.task),
       };
     }
     case 'EDIT_TASK_SUCCEEDED': {
       const { payload } = action;
-      return {
-        tasks: state.tasks.map(task => {
-          if (task.id === payload.task.id) {
-            return payload.task;
-          }
+      const nextTasks = state.tasks.map(task => {
+        if (task.id === payload.task.id) {
+          return payload.task;
+        }
 
-          return task;
-        }),
+        return task;
+      });
+      return {
+        ...state,
+        tasks: nextTasks,
       };
     }
     default: {
